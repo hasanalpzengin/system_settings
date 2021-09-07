562be02768b7dde02b016bb90b73647a09ea8ea7
Hasan Alp Zengin <hasalp38@gmail.com>
2021-09-07 22:40:29 +0200

    new task changed to show in recent

--- android/src/main/kotlin/de/florianweinaug/system_settings/SystemSettingsPlugin.kt
+++ android/src/main/kotlin/de/florianweinaug/system_settings/SystemSettingsPlugin.kt
@@ -22,45 +22,46 @@ public class SystemSettingsPlugin(private val registrar: Registrar): MethodCallH
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    var newTask: Boolean = call.argument<Boolean>("androidNewTask")!!
+    var showInRecents: Boolean = call.argument<Boolean>("showInRecents")!!
     when (call.method) {
-      "app"                 -> openAppSettings(newTask)
-      "app-notifications"   -> openAppNotificationSettings(newTask)
-      "system"              -> openSystemSettings(newTask)
-      "location"            -> openSetting(Settings.ACTION_LOCATION_SOURCE_SETTINGS, newTask)
-      "wifi"                -> openSetting(Settings.ACTION_WIFI_SETTINGS, newTask)
-      "bluetooth"           -> openSetting(Settings.ACTION_BLUETOOTH_SETTINGS, newTask)
-      "security"            -> openSetting(Settings.ACTION_SECURITY_SETTINGS, newTask)
-      "display"             -> openSetting(Settings.ACTION_DISPLAY_SETTINGS, newTask)
-      "date"                -> openSetting(Settings.ACTION_DATE_SETTINGS, newTask)
-      "sound"               -> openSetting(Settings.ACTION_SOUND_SETTINGS, newTask)
-      "apps"                -> openSetting(Settings.ACTION_APPLICATION_SETTINGS, newTask)
-      "wireless"            -> openSetting(Settings.ACTION_WIRELESS_SETTINGS, newTask)
-      "device-info"         -> openSetting(Settings.ACTION_DEVICE_INFO_SETTINGS, newTask)
-      "data-usage"          -> openSetting(Settings.ACTION_DATA_USAGE_SETTINGS, newTask)
-      "data-roaming"        -> openSetting(Settings.ACTION_DATA_ROAMING_SETTINGS, newTask)
-      "locale"              -> openSetting(Settings.ACTION_LOCALE_SETTINGS, newTask)
-      "default-apps"        -> openSetting(Settings.ACTION_MANAGE_DEFAULT_APPS_SETTINGS, newTask)
-      "airplane-mode"       -> openSetting(Settings.ACTION_AIRPLANE_MODE_SETTINGS, newTask)
-      "privacy"             -> openSetting(Settings.ACTION_PRIVACY_SETTINGS, newTask)
-      "accessibility"       -> openSetting(Settings.ACTION_ACCESSIBILITY_SETTINGS, newTask)
-      "internal-storage"    -> openSetting(Settings.ACTION_INTERNAL_STORAGE_SETTINGS, newTask)
-      "notification-policy" -> openSetting(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS, newTask)
-      "nfc"                 -> openSetting(Settings.ACTION_NFC_SETTINGS, newTask)
+      "app"                 -> openAppSettings(showInRecents)
+      "app-notifications"   -> openAppNotificationSettings(showInRecents)
+      "system"              -> openSystemSettings(showInRecents)
+      "location"            -> openSetting(Settings.ACTION_LOCATION_SOURCE_SETTINGS, showInRecents)
+      "wifi"                -> openSetting(Settings.ACTION_WIFI_SETTINGS, showInRecents)
+      "bluetooth"           -> openSetting(Settings.ACTION_BLUETOOTH_SETTINGS, showInRecents)
+      "security"            -> openSetting(Settings.ACTION_SECURITY_SETTINGS, showInRecents)
+      "display"             -> openSetting(Settings.ACTION_DISPLAY_SETTINGS, showInRecents)
+      "date"                -> openSetting(Settings.ACTION_DATE_SETTINGS, showInRecents)
+      "sound"               -> openSetting(Settings.ACTION_SOUND_SETTINGS, showInRecents)
+      "apps"                -> openSetting(Settings.ACTION_APPLICATION_SETTINGS, showInRecents)
+      "wireless"            -> openSetting(Settings.ACTION_WIRELESS_SETTINGS, showInRecents)
+      "device-info"         -> openSetting(Settings.ACTION_DEVICE_INFO_SETTINGS, showInRecents)
+      "data-usage"          -> openSetting(Settings.ACTION_DATA_USAGE_SETTINGS, showInRecents)
+      "data-roaming"        -> openSetting(Settings.ACTION_DATA_ROAMING_SETTINGS, showInRecents)
+      "locale"              -> openSetting(Settings.ACTION_LOCALE_SETTINGS, showInRecents)
+      "default-apps"        -> openSetting(Settings.ACTION_MANAGE_DEFAULT_APPS_SETTINGS, showInRecents)
+      "airplane-mode"       -> openSetting(Settings.ACTION_AIRPLANE_MODE_SETTINGS, showInRecents)
+      "privacy"             -> openSetting(Settings.ACTION_PRIVACY_SETTINGS, showInRecents)
+      "accessibility"       -> openSetting(Settings.ACTION_ACCESSIBILITY_SETTINGS, showInRecents)
+      "internal-storage"    -> openSetting(Settings.ACTION_INTERNAL_STORAGE_SETTINGS, showInRecents)
+      "notification-policy" -> openSetting(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS, showInRecents)
+      "nfc"                 -> openSetting(Settings.ACTION_NFC_SETTINGS, showInRecents)
       else                  -> result.notImplemented()
     }
   }
 
-  private fun openAppSettings(newTask: Boolean) {
+  private fun openAppSettings(showInRecents: Boolean) {
     val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
 
     val uri = Uri.fromParts("package", registrar.context().packageName, null)
     intent.data = uri
-    if(newTask){ intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) }
+    if(!showInRecents){ intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS); }
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
     registrar.context().startActivity(intent)
   }
 
-  private fun openAppNotificationSettings(newTask: Boolean) {
+  private fun openAppNotificationSettings(showInRecents: Boolean) {
     val intent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
       Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
               .putExtra(Settings.EXTRA_APP_PACKAGE, registrar.context().packageName)
@@ -68,23 +69,26 @@ public class SystemSettingsPlugin(private val registrar: Registrar): MethodCallH
       Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
               .setData(Uri.parse("package:${registrar.context().packageName}"))
     }
-    if(newTask){ intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) }
+    if(!showInRecents){ intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS); }
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
     registrar.context().startActivity(intent)
   }
 
-  private fun openSetting(name: String, newTask: Boolean) {
+  private fun openSetting(name: String, showInRecents: Boolean) {
     try {
       var intent: Intent = Intent(name);
-      if(newTask){ intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) }
+      if(!showInRecents){ intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS); }
+      intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
       registrar.context().startActivity(intent)
     } catch (e: Exception) {
-      openSystemSettings(newTask)
+      openSystemSettings(showInRecents)
     }
   }
 
-  private fun openSystemSettings(newTask: Boolean) {
+  private fun openSystemSettings(showInRecents: Boolean) {
     var intent: Intent = Intent(Settings.ACTION_SETTINGS);
-    if(newTask){ intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK) }
+    if(!showInRecents){ intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS); }
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
     registrar.context().startActivity(intent)
   }
 }
--- lib/system_settings.dart
+++ lib/system_settings.dart
@@ -5,95 +5,95 @@ import 'package:flutter/services.dart';
 class SystemSettings {
   static const MethodChannel _channel = const MethodChannel('system_settings');
 
-  static Future<void> app({androidNewTask=true}) async {
-    return await _channel.invokeMethod('app', {"androidNewTask": androidNewTask});
+  static Future<void> app({showInRecents=true}) async {
+    return await _channel.invokeMethod('app', {"showInRecents": showInRecents});
   }
 
-  static Future<void> appNotifications({androidNewTask=true}) async {
-    return await _channel.invokeMethod('app-notifications', {"androidNewTask": androidNewTask});
+  static Future<void> appNotifications({showInRecents=true}) async {
+    return await _channel.invokeMethod('app-notifications', {"showInRecents": showInRecents});
   }
 
-  static Future<void> system({androidNewTask=true}) async {
-    return await _channel.invokeMethod('system', {"androidNewTask": androidNewTask});
+  static Future<void> system({showInRecents=true}) async {
+    return await _channel.invokeMethod('system', {"showInRecents": showInRecents});
   }
 
-  static Future<void> location({androidNewTask=true}) async {
-    return await _channel.invokeMethod('location', {"androidNewTask": androidNewTask});
+  static Future<void> location({showInRecents=true}) async {
+    return await _channel.invokeMethod('location', {"showInRecents": showInRecents});
   }
 
-  static Future<void> wifi({androidNewTask=true}) async {
-    return await _channel.invokeMethod('wifi', {"androidNewTask": androidNewTask});
+  static Future<void> wifi({showInRecents=true}) async {
+    return await _channel.invokeMethod('wifi', {"showInRecents": showInRecents});
   }
 
-  static Future<void> bluetooth({androidNewTask=true}) async {
-    return await _channel.invokeMethod('bluetooth', {"androidNewTask": androidNewTask});
+  static Future<void> bluetooth({showInRecents=true}) async {
+    return await _channel.invokeMethod('bluetooth', {"showInRecents": showInRecents});
   }
 
-  static Future<void> security({androidNewTask=true}) async {
-    return await _channel.invokeMethod('security', {"androidNewTask": androidNewTask});
+  static Future<void> security({showInRecents=true}) async {
+    return await _channel.invokeMethod('security', {"showInRecents": showInRecents});
   }
 
-  static Future<void> display({androidNewTask=true}) async {
-    return await _channel.invokeMethod('display', {"androidNewTask": androidNewTask});
+  static Future<void> display({showInRecents=true}) async {
+    return await _channel.invokeMethod('display', {"showInRecents": showInRecents});
   }
 
-  static Future<void> date({androidNewTask=true}) async {
-    return await _channel.invokeMethod('date', {"androidNewTask": androidNewTask});
+  static Future<void> date({showInRecents=true}) async {
+    return await _channel.invokeMethod('date', {"showInRecents": showInRecents});
   }
 
-  static Future<void> sound({androidNewTask=true}) async {
-    return await _channel.invokeMethod('sound', {"androidNewTask": androidNewTask});
+  static Future<void> sound({showInRecents=true}) async {
+    return await _channel.invokeMethod('sound', {"showInRecents": showInRecents});
   }
 
-  static Future<void> apps({androidNewTask=true}) async {
-    return await _channel.invokeMethod('apps', {"androidNewTask": androidNewTask});
+  static Future<void> apps({showInRecents=true}) async {
+    return await _channel.invokeMethod('apps', {"showInRecents": showInRecents});
   }
 
-  static Future<void> wireless({androidNewTask=true}) async {
-    return await _channel.invokeMethod('wireless', {"androidNewTask": androidNewTask});
+  static Future<void> wireless({showInRecents=true}) async {
+    return await _channel.invokeMethod('wireless', {"showInRecents": showInRecents});
   }
 
-  static Future<void> deviceInfo({androidNewTask=true}) async {
-    return await _channel.invokeMethod('device-info', {"androidNewTask": androidNewTask});
+  static Future<void> deviceInfo({showInRecents=true}) async {
+    return await _channel.invokeMethod('device-info', {"showInRecents": showInRecents});
   }
 
-  static Future<void> dataUsage({androidNewTask=true}) async {
-    return await _channel.invokeMethod('data-usage', {"androidNewTask": androidNewTask});
+  static Future<void> dataUsage({showInRecents=true}) async {
+    return await _channel.invokeMethod('data-usage', {"showInRecents": showInRecents});
   }
 
-  static Future<void> dataRoaming({androidNewTask=true}) async {
-    return await _channel.invokeMethod('data-roaming', {"androidNewTask": androidNewTask});
+  static Future<void> dataRoaming({showInRecents=true}) async {
+    return await _channel.invokeMethod('data-roaming', {"showInRecents": showInRecents});
   }
 
-  static Future<void> locale({androidNewTask=true}) async {
-    return await _channel.invokeMethod('locale', {"androidNewTask": androidNewTask});
+  static Future<void> locale({showInRecents=true}) async {
+    return await _channel.invokeMethod('locale', {"showInRecents": showInRecents});
   }
 
-  static Future<void> defaultApps({androidNewTask=true}) async {
-    return await _channel.invokeMethod('default-apps', {"androidNewTask": androidNewTask});
+  static Future<void> defaultApps({showInRecents=true}) async {
+    return await _channel.invokeMethod('default-apps', {"showInRecents": showInRecents});
   }
 
-  static Future<void> airplaneMode({androidNewTask=true}) async {
-    return await _channel.invokeMethod('airplane-mode', {"androidNewTask": androidNewTask});
+  static Future<void> airplaneMode({showInRecents=true}) async {
+    return await _channel.invokeMethod('airplane-mode', {"showInRecents": showInRecents});
   }
 
-  static Future<void> privacy({androidNewTask=true}) async {
-    return await _channel.invokeMethod('privacy', {"androidNewTask": androidNewTask});
+  static Future<void> privacy({showInRecents=true}) async {
+    return await _channel.invokeMethod('privacy', {"showInRecents": showInRecents});
   }
 
-  static Future<void> accessibility({androidNewTask=true}) async {
-    return await _channel.invokeMethod('accessibility', {"androidNewTask": androidNewTask});
+  static Future<void> accessibility({showInRecents=true}) async {
+    return await _channel.invokeMethod('accessibility', {"showInRecents": showInRecents});
   }
 
-  static Future<void> internalStorage({androidNewTask=true}) async {
-    return await _channel.invokeMethod('internal-storage', {"androidNewTask": androidNewTask});
+  static Future<void> internalStorage({showInRecents=true}) async {
+    return await _channel.invokeMethod('internal-storage', {"showInRecents": showInRecents});
   }
 
-  static Future<void> nfc({androidNewTask=true}) async {
-    return await _channel.invokeMethod('nfc', {"androidNewTask": androidNewTask});
+  static Future<void> nfc({showInRecents=true}) async {
+    return await _channel.invokeMethod('nfc', {"showInRecents": showInRecents});
   }
 
-  static Future<void> notificationPolicy({androidNewTask=true}) async {
-    return await _channel.invokeMethod('notification-policy', {"androidNewTask": androidNewTask});
+  static Future<void> notificationPolicy({showInRecents=true}) async {
+    return await _channel.invokeMethod('notification-policy', {"showInRecents": showInRecents});
   }
 }
